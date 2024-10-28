934092723db5668e7d1746074dad919428157703
ahmedFahmy2024 <159808236+ahmedFahmy2024@users.noreply.github.com>
2024-10-28 22:00:45 +0300

    website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,56 @@
-export default function Home() {
+import AddComponent from "@/components/website/AddComponent";
+import CardComponenet from "@/components/website/CardComponenet";
+import { AUCTIONS, BASE_URL } from "@/server/Api";
+
+async function fetchAuction() {
+  try {
+    const response = await fetch(`${BASE_URL}${AUCTIONS}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch auction");
+    }
+
+    const auction = await response.json();
+    return auction.data;
+  } catch (error) {
+    console.error("Error fetching Auction:", error);
+    throw error;
+  }
+}
+
+type AuctionData = {
+  _id: string;
+  titleKey: string;
+  titleValue: string;
+  rightLogoValue: string;
+  leftLogoValue: string;
+  imageValue: string[];
+  videoKey: string;
+  videoValue: string;
+  dateStart: string;
+  imageCover: string;
+};
+
+export default async function Home() {
+  const data = await fetchAuction();
+  console.log(data);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p>This is a website</p>
-    </div>
+    <main>
+      <div className="container mx-auto pt-10">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {data.map((item: AuctionData) => (
+            <CardComponenet key={item._id} item={item} />
+          ))}
+
+          <AddComponent />
+        </div>
+      </div>
+    </main>
   );
 }
